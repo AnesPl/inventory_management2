dca785bfd73e090c7b259b7b158151c427cd8d74
AnesPl <anes.plancic@scv.si>
2025-03-31 13:24:30 +0200

    image upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,22 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :stock_transactions, dependent: :destroy
 
+  has_one_attached :image
+
+  validate :acceptable_image
+
+  def acceptable_image
+    return unless image.attached?
+
+    unless image.content_type.in?(%w[image/jpeg image/png image/gif])
+      errors.add(:image, 'must be a JPEG, PNG, or GIF')
+    end
+
+    if image.byte_size > 5.megabytes
+      errors.add(:image, 'is too big. Maximum size is 5MB')
+    end
+  end
+
   def total_stock
     stock_transactions.sum("CASE WHEN transaction_type = 'add' THEN quantity ELSE -quantity END") + stock
   end
